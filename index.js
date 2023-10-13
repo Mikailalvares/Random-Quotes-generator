a2c222c2386fa17a6e5eb6796743a2abd7188c24
Mikailalvares <mikailismail260@gmail.com>
2023-10-12 19:30:08 -0900

    add separate function for each event listener

--- index.js
+++ index.js
@@ -11,18 +11,49 @@ document.addEventListener("DOMContentLoaded", () => {
     const commentsList = document.getElementById("commentsList");
     const getQuoteButton = document.getElementById("getQuoteButton");
     const prevQuoteButton = document.getElementById("prevQuoteButton");
+    const quoteImage = document.getElementById("quoteImage");
 
     const quoteContainer = document.getElementById("quoteContainer");
-    const quoteImage = document.createElement("img");
-    quoteImage.id = "quoteImage";
-    quoteImage.alt = "Quote Image";
-    quoteContainer.appendChild(quoteImage);
 
     // Initialize quotes data from localStorage or set an empty array if it doesn't exist
     let quotes = JSON.parse(localStorage.getItem("quotes")) || [];
     let currentQuoteIndex = -1;
 
-    likeButton.addEventListener("click", () => {
+    // Function to fetch a random quote
+    function fetchRandomQuote() {
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                const { content, author: quoteAuthor } = data;
+                quoteText.textContent = content;
+                author.textContent = `- ${quoteAuthor}`;
+
+                // Fetch a random image for the quote
+                fetch(unsplashUrl)
+                    .then((response) => {
+                        quoteImage.style.backgroundImage = `url(${response.url})`;
+                    })
+                    .catch((error) => {
+                        console.error("Error fetching image:", error);
+                    });
+
+                // Update the current quote index and save it to localStorage
+                currentQuoteIndex = quotes.length;
+                quotes.push({ content, likeCount: 0, comments: [] });
+                likeCountSpan.textContent = quotes[currentQuoteIndex].likeCount;
+                commentsList.innerHTML = "";
+
+                // Save the updated quotes data to localStorage
+                localStorage.setItem("quotes", JSON.stringify(quotes));
+            })
+            .catch((error) => {
+                console.error("Error fetching quote:", error);
+                // You can display an error message to the user here
+            });
+    }
+
+    // Function to handle liking a quote
+    function handleLike() {
         if (currentQuoteIndex >= 0) {
             quotes[currentQuoteIndex].likeCount++;
             likeCountSpan.textContent = quotes[currentQuoteIndex].likeCount;
@@ -30,9 +61,10 @@ document.addEventListener("DOMContentLoaded", () => {
             // Save the updated quotes data to localStorage
             localStorage.setItem("quotes", JSON.stringify(quotes));
         }
-    });
+    }
 
-    addCommentButton.addEventListener("click", () => {
+    // Function to handle adding a comment
+    function handleComment() {
         const comment = commentInput.value;
         if (comment.trim() !== "" && currentQuoteIndex >= 0) {
             quotes[currentQuoteIndex].comments.push(comment);
@@ -44,12 +76,12 @@ document.addEventListener("DOMContentLoaded", () => {
             // Save the updated quotes data to localStorage
             localStorage.setItem("quotes", JSON.stringify(quotes));
         }
-    });
+    }
 
-    getQuoteButton.addEventListener("click", () => {
-        fetchRandomQuote();
-    });
-
+    // Event listeners
+    likeButton.addEventListener("click", handleLike);
+    addCommentButton.addEventListener("click", handleComment);
+    getQuoteButton.addEventListener("click", fetchRandomQuote);
     prevQuoteButton.addEventListener("click", () => {
         if (currentQuoteIndex > 0) {
             currentQuoteIndex--;
@@ -59,47 +91,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    function fetchRandomQuote() {
-        fetch(apiUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                const { content, author: quoteAuthor } = data;
-                quoteText.textContent = content;
-                author.textContent = `- ${quoteAuthor}`;
-
-                fetch(unsplashUrl)
-                    .then((response) => {
-                        quoteImage.src = response.url;
-                    })
-                    .catch((error) => {
-                        console.error("Error fetching image:", error);
-                    });
-
-                currentQuoteIndex = quotes.length;
-                quotes.push({ content, likeCount: 0, comments: [] });
-                likeCountSpan.textContent = quotes[currentQuoteIndex].likeCount;
-                commentsList.innerHTML = "";
-
-                // Save the updated quotes data to localStorage
-                localStorage.setItem("quotes", JSON.stringify(quotes));
-            })
-            .catch((error) => {
-                console.error("Error fetching quote:", error);
-            });
-    }
-
-    function displayQuote(index) {
-        if (index >= 0 && index < quotes.length) {
-            quoteText.textContent = quotes[index].content;
-            likeCountSpan.textContent = quotes[index].likeCount;
-            commentsList.innerHTML = "";
-            quotes[index].comments.forEach((comment) => {
-                const listItem = document.createElement("li");
-                listItem.textContent = comment;
-                commentsList.appendChild(listItem);
-            });
-        }
-    }
-
+    // Fetch a random quote when the page loads
     fetchRandomQuote();
 });
